b2a2de76360ca612eb4dcb6cc013eed323f0fe67
kk <kkjiweb@gmail.com>
2021-10-03 10:31:19 +0530

    start

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { SSL_OP_TLS_BLOCK_PADDING_BUG } = require('constants')
 app.use(express.static(path.join(__dirname , "static")))
 
 app.use('/', require(path.join(__dirname, 'routes/home.js')))
-app.use('/about', require(path.join(__dirname, 'routes/about.js')))
+// app.use('/about', require(path.join(__dirname, 'routes/about.js')))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
